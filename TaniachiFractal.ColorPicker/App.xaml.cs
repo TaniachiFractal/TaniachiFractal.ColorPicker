da07a758929fbf53859ead8be3e5e0791258b27b
TaniachiFractal <tnmslv@yandex.ru>
2025-02-02 12:18:51 +0300

    Add project files.

---
+++ TaniachiFractal.ColorPicker/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace TaniachiFractal.ColorPicker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
